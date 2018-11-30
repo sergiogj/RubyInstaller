e125387aa4e6f51fb02baffa4e07311dc69c62e9
sergiogj <sergiogj@users.noreply.github.com>
2018-11-30 11:43:44 +0100

    Update ruby.sh

--- ruby.sh
+++ ruby.sh
@@ -5,15 +5,15 @@ sudo apt-get update
 sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
 cd
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-echo "\n" >> ~/.bashrc
+
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 exec $SHELL
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
 exec $SHELL
-rbenv install 2.3.1
-rbenv global 2.3.1
+rbenv install 2.5.3
+rbenv global 2.5.3
 ruby -v
 gem install bundler
 rbenv rehash
@@ -32,5 +32,5 @@ cat ~/.ssh/id_rsa.pub
 echo "Presiona una tecla para continuar" ;read -p ""
 ssh -T git@github.com
 #Instalar Nodejs
-curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
